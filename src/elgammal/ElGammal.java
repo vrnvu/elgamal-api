7c3820b3a6e6f06388dd428c9a83675eabcb4590
Arnau Diaz <diazarnau0@gmail.com>
2018-04-16 13:52:54 +0200

    ElGammal implements crypto

--- src/elgammal/ElGammal.java
+++ src/elgammal/ElGammal.java
@@ -1,4 +1,15 @@
 package elgammal;
 
-public class ElGammal  {
+import java.math.BigInteger;
+
+public class ElGammal implements Cryptosystem{
+    @Override
+    public Vote encrypt(BigInteger plain) {
+        return null;
+    }
+
+    @Override
+    public BigInteger decrypt(Vote vote) {
+        return null;
+    }
 }
