a70553fc4dc5d00a736079de1b85e6e2e84c6914
Arnau Diaz <diazarnau0@gmail.com>
2018-04-17 15:27:29 +0200

    TODO: format readme

--- src/elgamal/ElGamal.java
+++ src/elgamal/ElGamal.java
@@ -1,17 +1,39 @@
 package elgamal;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
 
 public class ElGamal implements Cryptosystem {
 
     private final BigInteger privateKey;
-    private final BigInteger g;
-    private final BigInteger q;
+    private final BigInteger generator;
+    private final BigInteger order;
+    private final BigInteger grade;
+
+
+    private static final BigInteger ZERO = BigInteger.ZERO;
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
 
     public ElGamal() {
+        generateCyclicGroup();
+        generatePrivateKey();
         privateKey = null;
-        g = null;
-        q = null;
+        generator = null;
+        order = null;
+        grade = null;
+
+    }
+
+    private void generatePrivateKey() {
+
+    }
+
+    private void generateCyclicGroup() {
+        // BigInteger(int bitLength, int certainty, Random rnd)
+        Random r = new SecureRandom();
+        BigInteger b = new BigInteger(5,20, r);
     }
 
     @Override
