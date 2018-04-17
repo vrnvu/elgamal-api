2efdb43fe46e42dfc137a3eb60ccbcb5a4c5c7a7
Arnau Diaz <diazarnau0@gmail.com>
2018-04-17 17:45:55 +0200

    Get random g from Fp

--- src/elgamal/ElGamal.java
+++ src/elgamal/ElGamal.java
@@ -3,6 +3,7 @@ package elgamal;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ElGamal implements Cryptosystem {
 
@@ -11,42 +12,9 @@ public class ElGamal implements Cryptosystem {
     private BigInteger order;
     private BigInteger grade;
 
-
-    private static final BigInteger ZERO = BigInteger.ZERO;
     private static final BigInteger ONE = BigInteger.ONE;
     private static final BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
     private Random r;
-
-    /*
-           BigInteger dos= BigInteger.ONE.add(BigInteger.ONE);
-        BigInteger aux= BigInteger.ONE;
-        Boolean correcte = false;
-        Random rnd = new SecureRandom();
-        while(!correcte){
-            q = new BigInteger(grauSeguretat, 20, rnd);
-            grau_Z= dos.multiply(q).add(BigInteger.ONE);
-            correcte=grau_Z.isProbablePrime(20);
-            //System.out.println("perdent temps");
-        }
-        correcte= false;
-        while(!correcte){
-            g= new BigInteger(25,rnd);
-            g=g.mod(grau_Z.subtract(BigInteger.ONE));
-            if(!g.modPow(dos, grau_Z).equals(BigInteger.ONE)){
-                correcte=true;
-            }
-            //System.out.println("perdo temps");
-        }
-
-        privatekey= new BigInteger(100,rnd);
-        privatekey= privatekey.mod(q);
-        encriptador= new EncriptadorGamalAdd(getClauPublica(),g,grau_Z,q);
-        resCalculats = new BigInteger[10];
-        for(int i=0; i<10; i++){
-            aux= aux.add(BigInteger.ONE);
-            resCalculats[i]=g.modPow(aux, grau_Z);
-        }
-     */
 
 
     // https://crypto.stackexchange.com/questions/1451/elgamal-multiplicative-cyclic-group-and-key-generation?rq=1
@@ -71,15 +39,16 @@ public class ElGamal implements Cryptosystem {
 
         boolean gradeIsPrime;
         do {
-            order = new BigInteger(4, 30, r);
+            order = new BigInteger(3, 10, r);
             grade = TWO.multiply(order).add(BigInteger.ONE);
-            gradeIsPrime = grade.isProbablePrime(30);
+            gradeIsPrime = grade.isProbablePrime(10);
             } while (!gradeIsPrime);
 
         boolean isGenerator;
         do {
             // How to efficient find a generator?
-            generator = new BigInteger(4, r);
+            int randomNum = ThreadLocalRandom.current().nextInt(0, grade.subtract(ONE).intValue());
+            generator = new BigInteger(String.valueOf(randomNum));
             // Choose an element g of Fp with order n
             isGenerator = generator.modPow(order, grade).equals(ONE);
         } while (!isGenerator && !generator.modPow(TWO, grade).equals(ONE));
