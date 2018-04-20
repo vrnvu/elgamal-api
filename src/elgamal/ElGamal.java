c4e151e9978c115ab7aba86fa374e1332de14707
Arnau Diaz <diazarnau0@gmail.com>
2018-04-20 16:08:44 +0200

    El Gamal encrypt/decrypt

--- src/elgamal/ElGamal.java
+++ src/elgamal/ElGamal.java
@@ -9,7 +9,7 @@ public class ElGamal implements Cryptosystem {
 
     private BigInteger x;
     private BigInteger h;
-    private BigInteger generator;
+    private BigInteger g;
     private BigInteger order;
     private BigInteger grade;
 
@@ -26,69 +26,62 @@ public class ElGamal implements Cryptosystem {
     }
 
     private void generatePrivateKey() {
-        int r = ThreadLocalRandom.current().nextInt(1, grade.subtract(ONE).intValue());
-        x = new BigInteger(String.valueOf(r)).mod(order);
-        h = generator.pow(x.intValue());
+        int r = ThreadLocalRandom.current().nextInt(0, grade.subtract(ONE).intValue());
+        x = new BigInteger(String.valueOf(r));
+        h = g.modPow(x, grade);
     }
 
     private void generateCyclicGroup() {
-        // BigInteger(int bitLength, int certainty, Random rnd)
-        // isProbablePrime(int certainty)
-
         boolean gradeIsPrime;
         do {
-            order = new BigInteger(6, 10, r);
+            order = new BigInteger(3, 10, r);
             grade = TWO.multiply(order).add(BigInteger.ONE);
             gradeIsPrime = grade.isProbablePrime(10);
             } while (!gradeIsPrime);
 
-        boolean isGenerator;
         do {
-            // How to efficient find a generator?
-            int randomNum = ThreadLocalRandom.current().nextInt(0, grade.subtract(ONE).intValue());
-            generator = new BigInteger(String.valueOf(randomNum));
-            // Choose an element g of Fp with order n
-            isGenerator = generator.modPow(order, grade).equals(ONE);
-        } while (!generator.modPow(TWO, grade).equals(ONE));
+            int randomNum = ThreadLocalRandom.current().nextInt(2, grade.subtract(ONE).intValue());
+            g = new BigInteger(String.valueOf(randomNum));
+        } while (g.modPow(TWO, grade).equals(ONE));
 
+        if(!g.modPow(order, grade).equals(ONE)) {
+            g = g.modPow(TWO, grade);
+        }
+
+    }
+
+    public void printGroupDetails() {
+        System.out.println(String.format("order=%d grade=%d g=%d h=%d",
+                this.order.intValue(), this.grade.intValue(), this.g.intValue(),
+                this.h.intValue(),  this.x.intValue()));
     }
 
     @Override
     public Vote encrypt(BigInteger plain) {
-        int y = ThreadLocalRandom.current().nextInt(1, grade.subtract(ONE).intValue());
-        BigInteger c1 = generator.pow(y);
-        BigInteger s = h.pow(y);
-        // Map message "plain" to an element m' of G
-        BigInteger c2 = plain.multiply(s);
+        BigInteger m = plain.mod(grade);
+        BigInteger random = BigInteger.valueOf(ThreadLocalRandom.current().nextInt(2, grade.subtract(ONE).intValue()));
+        BigInteger c1 = g.modPow(random, grade);
+        BigInteger s = h.modPow(random, grade);
+        BigInteger c2 = m.multiply(s).mod(grade);
         return new Vote(c1, c2);
     }
 
     @Override
     public BigInteger decrypt(Vote vote) {
-        // When is needed?
-
-        BigInteger s = vote.getC1().pow(x.intValue());
-        //BigInteger m1 = vote.getC2().pow(s.pow(-1).intValue());
-        // Optimization consequence of Lagrange Theorem
-        // inverse_ s = c1 ^ (q-x) = g ^ (q-x)y
-        BigInteger inverse_s = vote.getC1().pow((grade.subtract(new BigInteger(String.valueOf(x))).mod(order)).intValue());
-        return inverse_s.multiply(vote.getC2());
+        BigInteger inverse_s = vote.getC1().modPow(order.subtract(x), grade);
+        return inverse_s.multiply(vote.getC2()).mod(grade);
     }
 
     public static void main(String[] args) {
         ElGamal gamal = new ElGamal();
+        gamal.printGroupDetails();
+
         BigInteger message1 = new BigInteger("1");
         System.out.println("Before encryption: " + message1.toString());
-        System.out.println(String.format("%d %d %d %d", gamal.order, gamal.grade, gamal.generator, gamal.h,  gamal.x));
-
-        BigInteger message2 = new BigInteger("2");
+        BigInteger message2 = new BigInteger("11");
         System.out.println("Before encryption: " + message2.toString());
-        System.out.println(String.format("%d %d %d %d", gamal.order, gamal.grade, gamal.generator, gamal.h,  gamal.x));
-
         BigInteger message3 = new BigInteger("3");
         System.out.println("Before encryption: " + message3.toString());
-        System.out.println(String.format("%d %d %d %d", gamal.order, gamal.grade, gamal.generator, gamal.h,  gamal.x));
-
         Vote v1 = gamal.encrypt(message1);
         Vote v2 = gamal.encrypt(message2);
         Vote v3 = gamal.encrypt(message3);
